aa717835430aab26487a9563d80f873de0d387bf
feizai <c1376479276@163.com>
2021-11-01 11:51:43 +0800

    新增实体类

---
+++ app/src/main/java/com/lenovo/feizai/networkframe/entity/User.java
@@ -0,0 +1,13 @@
+package com.lenovo.feizai.networkframe.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String account;
+    private String password;
+}
